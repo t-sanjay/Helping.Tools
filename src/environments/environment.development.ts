350a6dc5eae4a474127842babed00332b4830dfb
SanjayOnCode <t-sanjay@hotmail.com>
2023-05-20 01:37:10 +0530

    removed keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,17 +1,3 @@
 export const environment = {
-    firebaseConfig: {
-        apiKey: "AIzaSyAO9ExMyV1G_UPnmUGSYVTFZBi09l3cHL0",
-        authDomain: "helpig-tools.firebaseapp.com",
-        projectId: "helpig-tools",
-        storageBucket: "helpig-tools.appspot.com",
-        messagingSenderId: "366914184796",
-        appId: "1:366914184796:web:9627fd1ff5cecc0e8b6899",
-        measurementId: "G-WRZSGQJEP5"
-      },
-      currencyKey : 'c59d3746bb0b7b7880ef9fd6',
-      openAi: {
-        env_name: '',
-        organisationId: 'org-Gw7PIYbNZGsGTjmLpPK14qHV',
-        apiKey: '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'
-      }
+   
 };
